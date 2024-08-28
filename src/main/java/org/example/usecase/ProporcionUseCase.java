e1d56d452691e156c58331f1aeb21a6501e98601
StevenValdivia <s.brandon.vm@gmail.com>
2024-08-28 16:01:40 -0500

    Calculos de Steven

--- src/main/java/org/example/usecase/ProporcionUseCase.java
+++ src/main/java/org/example/usecase/ProporcionUseCase.java
@@ -1,16 +1,45 @@
 package org.example.usecase;
 
-public class ProporcionUseCase implements ProporcionUseCaseInput
-{
+import org.example.domain.Proporcion;
+
+public class ProporcionUseCase implements ProporcionUseCaseInput{
+
     @Override
-    public ProporcionResponse calcular(ProporcionRequest request)
-    {
+    public ProporcionResponse calcular(ProporcionRequest request) {
+
+        int[] medidas = asegurarProporcion(request.getTotal());
+        int medida1 = medidas[0];
+        int medida2 = medidas[1];
+
+        int valorAjustado = calcularValorAjustado(request.getSeparador());
+
+        int[] espacios = calcularEspacios(medida1, medida2, valorAjustado);
+        int espacio1 = espacios[0];
+        int espacio2 = espacios[1];
+
         ProporcionResponse response = new ProporcionResponse();
-        response.setTotal(600);
-        response.setMedida1(373);
-        response.setMedida2(173);
-        response.setSeparador(18);
-        response.setEspacios(0);
+        response.setTotal(request.getTotal());
+        response.setMedida1(espacio1);
+        response.setMedida2(espacio2);
+        response.setSeparador(request.getSeparador());
+        response.setEspacios(valorAjustado);
+
         return response;
+    }
+
+    public int[] asegurarProporcion(int total) {
+        int medida2 = total / 3;
+        int medida1 = medida2 * 2;
+        return new int[]{medida1, medida2};
+    }
+
+    public int calcularValorAjustado(int separador) {
+        return (separador * 3) / 2;
+    }
+
+    public int[] calcularEspacios(int medida1, int medida2, int valorAjustado) {
+        int espacio1 = medida1 - valorAjustado;
+        int espacio2 = medida2 - valorAjustado;
+        return new int[]{espacio1, espacio2};
     }
 }
